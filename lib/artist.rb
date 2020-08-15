c59724f3e411651b1ec0c552824151ed94737e71
Austin Cummings <aac729@live.com>
2020-08-15 15:48:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,33 @@
+require 'pry'
 
+class Artist
+  # extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include PARAMABLE
+  # include Initialize
+  attr_accessor :name
+  attr_reader :songs
+
+  @@artists = []
+
+  def initialize
+    super
+    @songs = []
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+end
